0e65e9d84f4ce867b36c810fcdf5c0b8e7dfa73e
tcdw <tcdw2011@gmail.com>
2019-07-01 10:41:28 +0800

    home

--- src/index.js
+++ src/index.js
@@ -1,4 +1,37 @@
-import Index from './index.eft';
+import { inform, exec } from 'ef.js';
+import page from 'page';
+import Body from './compoments/body.eft';
+import Nav from './compoments/nav.eft';
+import Home from './compoments/home.eft';
+import HomeHeader from './compoments/home-header.eft';
+import 'normalize.css';
+import './sass/index.scss';
 
-const index = new Index();
-index.$mount({ target: document.body });
+const body = new Body();
+const nav = new Nav();
+const home = new Home();
+const homeHeader = new HomeHeader();
+body.nav = nav;
+
+page('/*', (ctx) => {
+    if (ctx.path === '/') {
+        page.redirect('home');
+        return;
+    }
+    page.redirect(ctx.path.replace(/^\/+/, ''));
+});
+
+page('home', () => {
+    inform();
+    home.header = homeHeader;
+    body.content = home;
+    exec();
+});
+
+page.exit('home', (ctx, next) => {
+    next();
+});
+
+page({ hashbang: true });
+
+body.$mount({ target: document.body, option: 'replace' });
