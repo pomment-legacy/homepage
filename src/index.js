1d6c17b0091deb040fc21143eb34afdb7f129edf
tcdw <tcdw2011@gmail.com>
2019-07-01 14:02:59 +0800

    布局

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import Home from './compoments/home.eft';
 import HomeHeader from './compoments/home-header.eft';
 import HomeInfo from './compoments/home-info.eft';
 import 'normalize.css';
+import './sass/fluidable.scss';
 import './sass/index.scss';
 
 const body = new Body();
