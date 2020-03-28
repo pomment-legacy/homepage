ea7d8100989cf88f51eadb7e8a67e4a0139852c7
tcdw <tcdw2011@gmail.com>
2020-03-28 23:16:56 +0800

    readd eslint, delete scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "no-underscore-dangle": ["error", {
-            "allowAfterThis": true,
-        }],
-        "indent": ["error", 4, {
-            "SwitchCase": 1
-        }],
-        "prefer-destructuring": "off",
-        "no-console": "off",
-        "no-param-reassign": "off"
-    },
-    "env": {
-        "browser": true
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "no-underscore-dangle": ["error", {
+        "allowAfterThis": true,
+    }],
+    "indent": ["error", 4, {
+        "SwitchCase": 1
+    }],
+    "prefer-destructuring": "off",
+    "no-console": "off",
+    "no-param-reassign": "off"
+  },
 };
